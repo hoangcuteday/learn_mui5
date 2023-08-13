4876743bc14a2865343d6249f8890effcc91039c
hoangcuteday <109096454+hoangcuteday@users.noreply.github.com>
2023-08-13 22:28:20 +0700

    feat(sidebar): create sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,99 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch
+} from "@mui/material";
+import {
+  Home,
+  Article,
+  People,
+  Storefront,
+  Person,
+  Settings,
+  AccountBox,
+  ModeNight,
+} from "@mui/icons-material";
 
 function Sidebar() {
   return (
-    <Box bgcolor="orange" flex="1" p={2}>
-      SideBar
+    <Box
+      bgcolor="white"
+      flex="1"
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText>Homepage</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText>Pages</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <People />
+              </ListItemIcon>
+              <ListItemText>Groups</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText>Marketplace</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText>Friends</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText>Settings</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText>Profile</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
